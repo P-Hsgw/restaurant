f9827a6ab240cce9715391254fcd562f8a0bc721
P-Hsgw <contact@p-hsgw.com>
2021-04-03 22:29:50 +0900

    Add menu.js function, that will create divs with food items

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,11 @@
 import loadHome from './home';
+import menu from "./menu"
 
 function initializeSite() {
+  const content = document.getElementById("content")
+
+  content.appendChild(menu())
+
   loadHome()
 }
 
---
+++ src/menu.js
@@ -0,0 +1,52 @@
+function createMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add("menu")
+
+  function createDiv(id, title, paragraph) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", id);
+    newDiv.classList.add("menu_box")
+
+    newDiv.appendChild(title);
+    newDiv.appendChild(paragraph);
+
+    menu.appendChild(newDiv)
+
+  }
+
+  function createTitle(content, id="tit") {
+    const newTitle = document.createElement("h2");
+    newTitle.setAttribute("id", id);
+
+    newTitle.innerHTML = content;
+
+    return newTitle;
+  }
+
+  function createContent(content, id="cont") {
+    const newContent = document.createElement("p");
+    newContent.setAttribute("id", id);
+
+    newContent.innerHTML = content;
+
+    return newContent;
+  }
+
+  createDiv(1, createTitle("Pizza"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(2, createTitle("Pasta"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(3, createTitle("Lasagne"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(4, createTitle("Pierogi"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(5, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(6, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(7, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(8, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(9, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+  createDiv(10, createTitle("Zapiekanki"), createContent("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi necessitatibus voluptatum aut. Eaque, praesentium labore?"));
+
+
+
+  return menu;
+
+}
+
+export default createMenu;
